e881f004921536c618d5d96bc2104633fd7b3fa2
leiyunhong <1124015891@qq.com>
2021-03-12 19:37:37 +0800

    修改词法分析器

--- src/pers/lomesome/compliation/view/mywidgets/MyCodeArea.java
+++ src/pers/lomesome/compliation/view/mywidgets/MyCodeArea.java
@@ -21,7 +21,6 @@ public class MyCodeArea extends CodeArea {
 
         this.setUserData(file);
         IntFunction<Node> graphicFactory = line -> {
-            System.out.println(line);
             Circle circle = new Circle(4);
             circle.setFill(Color.RED);
             circle.setStroke(Color.RED);
